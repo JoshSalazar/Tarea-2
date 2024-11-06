f5e83cd64502096fb7c293fb075831d2d6a51682
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-06 13:12:06 -0600

    Create Ejercicio 1-3.c

---
+++ Ejercicio 1-3.c
@@ -0,0 +1,14 @@
+/*En una Casa de Cabio necesitan construir un programa que dado como dato una cantidad expresada en dólares, convierta esa cantidad en pesos. Considere que 1 dolar = 11.96 pesos*/
+#include <stdio.h>
+
+int main()
+
+{
+
+  int dol = 0;
+  int pes = 0;
+
+  printf("Ingrese la cantidad en dólares. \n");
+  scanf("%d", &dol);
+
+  pes = dol * 11.69
