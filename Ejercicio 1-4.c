a5a1c179cae96b55f304cc2320cc583f209e415d
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-08 22:42:16 -0600

    Create Ejercicio 1-4.c

---
+++ Ejercicio 1-4.c
@@ -0,0 +1,19 @@
+/*Una persona invierte en un banco una determinada cantidad de dinero y a una cierta tasa de interés mensual. Elabora un código que permita obtener el monto de dinero que obtendrá al finalizar el mes.*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  int din = 0;
+  int inte = 0;
+  int res = 0;
+
+  printf("Ingrese la cantidad de dinero inicial. \n");
+  scanf("%d", &din);
+
+  printf("Ingrese la tasa de interés. \n");
+  scanf("%d", &inte);
+
+  res = 
