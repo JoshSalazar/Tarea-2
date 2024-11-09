d947960598e7f3c6a08f6b727a9ad96933e4594f
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-09 17:49:56 -0600

    Create Ejercicio 2-1.c

---
+++ Ejercicio 2-1.c
@@ -0,0 +1,39 @@
+/*En un negocio de productos electrodomésticos aplican un descuento del
+8% a todos aquellos clientes cuya compra es superior a $2,500. Dado como
+dato el monto de la compra del cliente, calcule lo que el cliente debe pagar.
+Hacer el diagrama de flujo correspondiente.*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  float cos = 0;
+  float desc = 0.08;
+  float total = 0; 
+
+  printf("Ingrese el costo total original de la compra.\n");
+  scanf("%f", &cos);
+
+  if(cos > 2500)
+
+  {  
+
+    total = cos - (cos * desc);
+
+    printf("El costo con descuento es: %f", total);
+
+  }
+
+  else
+    
+  {
+
+    printf("El costo es: %f", cos);
+
+  }
+
+  return 0;
+
+}
