ce83427e44434aa6439e11fb2df8550f883030e8
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-09 22:57:57 -0600

    Create Ejercicio 2-4.c

---
+++ Ejercicio 2-4.c
@@ -0,0 +1,37 @@
+/*Construir un diagrama de flujo que pueda determinar, dados dos números
+enteros, si un número es divisor del otro.*/
+
+#include <stdio.h>
+
+int main()
+
+{
+  
+  int n1 = 0;
+  int n2 = 0;
+
+  printf("Ingrese el primer número. \n");
+  scanf("%d", &n1);
+  printf("Ingrese el segundo número. \n");
+  scanf("%d", &n2);
+
+  if (n1 == 0 || n2 == 0)
+  {
+    printf("No se puede dividir entre 0. :)");
+  }
+  else if(n1 % n2 == 0)
+  {
+    printf("%d es divisor de %d", n2, n1);
+  }
+  else if(n2 % n1 == 0)
+  {
+    printf("%d es divisor de %d", n1, n2);
+  }
+  else
+  {
+    printf("Ningún número es divisor del otro.");
+  }
+
+  return 0;
+  
+}
