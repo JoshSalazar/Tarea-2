11f7d0cc8e74b64ac1663a2b0a27ae48709116a4
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-10 00:51:46 -0600

    Create Ejercicio 3-4.c

---
+++ Ejercicio 3-4.c
@@ -0,0 +1,17 @@
+/*Hacer un diagrama de flujo que calcule e imprima el producto de los N
+primeros números naturales.*/
+
+#include <stdio.h>
+
+int main()
+
+{
+  int N = 0;
+  int imp = 0;
+  int c = 0;
+  printf("Ingrese un número N \n");
+  scanf("%d", &N);
+  for(c = 0; c < N; c++)
+  {
+    
+}
