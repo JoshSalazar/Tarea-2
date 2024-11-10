5a52c59c6a80fe3c005072c219493549895fb989
JoshSalazar <Salazar5a10j2@outlook.com>
2024-11-09 23:33:21 -0600

    Create Ejercicio 3-1.c

---
+++ Ejercicio 3-1.c
@@ -0,0 +1,28 @@
+/*Haga un diagrama de flujo para obtener la tabla de multiplicar de un
+número entero N, comenzando desde el 1.*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+  int N = 0;
+  int T = 1;
+  int c;
+
+  printf("Ingrese un número \n");
+  scanf("%d", &N);
+
+  for(c = 1; c <= 10; c++)
+  {
+
+    T = N * c;
+
+      printf("%d * %d = %d \n", N, c, T);
+
+  }
+
+  return 0;
+  
+}
